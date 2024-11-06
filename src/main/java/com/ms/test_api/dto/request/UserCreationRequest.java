4733d10bd5dc88f96f8bdcb6d453480057f69540
minhson-git <minhsont2112@gmail.com>
2024-11-06 17:16:04 +0700

    update validation

--- src/main/java/com/ms/test_api/dto/request/UserCreationRequest.java
+++ src/main/java/com/ms/test_api/dto/request/UserCreationRequest.java
@@ -1,6 +1,7 @@
 package com.ms.test_api.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,12 +11,13 @@ public class UserCreationRequest {
     private String citizenId;
 
     @Size(min = 3, message = "Username must be at least 3 characters")
+    @NotEmpty
     private String username;
 
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
     
-    @Email
+    @Email(message = "Email is not in correct format")
     private String email;
 
     private String fullname;
