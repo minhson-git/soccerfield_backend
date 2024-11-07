ab5955a401d62c34f3f5fc239e2bae2262414c38
minhson-git <minhsont2112@gmail.com>
2024-11-07 21:59:38 +0700

    update feat role

--- src/main/java/com/ms/test_api/reponsitory/RoleRepository.java
+++ src/main/java/com/ms/test_api/reponsitory/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.ms.test_api.reponsitory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ms.test_api.modal.Role;
 
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
 
 }
