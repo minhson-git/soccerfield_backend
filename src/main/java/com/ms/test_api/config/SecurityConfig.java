46cf9d8c7aa7c57d8880194ac4595fafbc4d340a
NamPhuong104 <nguyennamphuong104@gmail.com>
2024-11-12 01:37:47 +0700

    update security

--- src/main/java/com/ms/test_api/config/SecurityConfig.java
+++ src/main/java/com/ms/test_api/config/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig {
         httpSecurity
                 .authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/branchs").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/users").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/roles").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated());
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
