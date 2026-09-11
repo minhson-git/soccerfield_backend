c0d004f486dc5b8995254b89bd913c7810d2110d
minhson-git <minhsont2112@gmail.com>
2026-09-11 16:32:32 +0700

    fix: enforce booking ownership and remove dead code

--- src/main/java/com/ms/test_api/exception/BookingConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.ms.test_api.exception;
-
-public class BookingConflictException extends RuntimeException {
-    public BookingConflictException(String message) {
-        super(message);
-    }
-}
